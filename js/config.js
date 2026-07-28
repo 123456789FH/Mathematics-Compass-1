c0dd7256d5d65d5cf01c23189f635e293a5e8d4a
123456789FH <212942390+123456789FH@users.noreply.github.com>
2026-07-28 21:03:53 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
 
+window.BOUSLA_CONFIG = {
+  SUPABASE_URL: "https://epcfboiyydlhmbzkfyts.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_UuGzSBOdg47FB-d4vIUAPg_kj4kQMTE
+
+",
+  DEMO_MODE: false,
+  APP_NAME: "بوصلة رياضيات ١"
+};
