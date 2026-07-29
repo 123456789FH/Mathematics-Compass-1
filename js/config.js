2d568df71072bdfb93334bfa4fb74e83a2b24dc0
123456789FH <212942390+123456789FH@users.noreply.github.com>
2026-07-29 08:13:30 +0300

    Fix syntax error in BOUSLA_CONFIG object

--- js/config.js
+++ js/config.js
@@ -1,9 +1,6 @@
-
 window.BOUSLA_CONFIG = {
   SUPABASE_URL: "https://epcfboiyydlhmbzkfyts.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_UuGzSBOdg47FB-d4vIUAPg_kj4kQMTE
-
-",
+  SUPABASE_ANON_KEY: "sb_publishable_UuGzSBOdg47FB-d4vIUAPg_kj4kQMTE",
   DEMO_MODE: false,
   APP_NAME: "بوصلة رياضيات ١"
-};
+};};
